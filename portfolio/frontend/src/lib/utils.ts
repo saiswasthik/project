4a0c6f825bc5d17f7ec9234d06e8b5bf08cdfe1c
saiswasthik <saiswasthikyadav8@gmail.com>
2025-06-17 15:53:16 +0530

    deleted

--- portfolio/frontend/src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-// Utility function for merging class names
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
