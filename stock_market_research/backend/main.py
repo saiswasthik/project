fee0baa1d59b277c3c57c19d54c9b46680d738d8
saiswasthik <saiswasthikyadav8@gmail.com>
2025-06-18 15:25:09 +0530

    Update main.py

--- stock_market_research/backend/main.py
+++ stock_market_research/backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Frontend URL
+    allow_origins=["http://localhost:5173","https://project-hvgn.vercel.app"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
