12184c6c510024ac251ae5f40351b5a6dc5580f5
saiswasthik <saiswasthikyadav8@gmail.com>
2025-06-18 14:59:16 +0530

    Update main.py

--- stock_market_research/backend/main.py
+++ stock_market_research/backend/main.py
@@ -32,4 +32,4 @@ import os
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 9000)))
