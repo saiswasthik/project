65b905d3e47f5ddb69fc4d0bfd55dfe5067c3e14
saiswasthik <saiswasthikyadav8@gmail.com>
2025-06-19 20:48:22 +0530

    Update main.py

--- token_calculator/backend/app/main.py
+++ token_calculator/backend/app/main.py
@@ -7,7 +7,8 @@ app = FastAPI(title="PDF Token Calculator")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ "http://localhost:3000","https://llm-token-calculator.onrender.com"],  # React app URL for local host "http://localhost:3000",
+    allow_origins=[ "http://localhost:3000",
+            "https://llm-token-calculator.onrender.com"],  # React app URL for local host "http://localhost:3000",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"], 
