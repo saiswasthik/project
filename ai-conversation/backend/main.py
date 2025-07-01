b27482ed4da4af7b1d36dad27aaa6420a3043453
saiswasthik <saiswasthikyadav8@gmail.com>
2025-07-01 17:17:47 +0530

    Update main.py

--- ai-conversation/backend/main.py
+++ ai-conversation/backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://conversation-zeta.vercel.app/"],
+    allow_origins=["https://conversation-zeta.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
