9eeb1d9882a524e42616b53ea8b1e2d0419bbd8e
saiswasthik <saiswasthikyadav8@gmail.com>
2026-02-22 14:24:44 +0530

    Update main.py

--- reservation/backend/app/main.py
+++ reservation/backend/app/main.py
@@ -14,12 +14,12 @@ app=FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "tablereservations-pi.vercel.app"
+    "https://tablereservations-pi.vercel.app"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,         
+    allow_origins=["*"],         
     allow_credentials=True,
     allow_methods=["*"],            
     allow_headers=["*"],           
